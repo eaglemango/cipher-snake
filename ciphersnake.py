3c6cf255a45e9c0588407adb2e2eef48a032d46f
Sergey Gorbunov <eaglemango@gmail.com>
2019-04-01 18:41:42 +0300

    Move actions from main part to separate functions

--- ciphersnake.py
+++ ciphersnake.py
@@ -10,15 +10,49 @@ parser.add_argument("-i", "--input-file", default=None,
 parser.add_argument("-o", "--output-file", default=None,
                     help="Choose the output file (if not chosen, text will be written to screen")
 
+
+def encrypt_action():
+    if book[args.cipher].need_key and args.key is None:
+        print("Sorry, but {0} needs a key to {1}".format(args.cipher, args.action))
+        exit(0)
+
+    if book[args.cipher].need_key:
+        temp_result = book[args.cipher].encrypt(text, args.key)
+    else:
+        temp_result = book[args.cipher].encrypt(text)
+
+    return temp_result
+
+
+def decrypt_action():
+    if book[args.cipher].need_key and args.key is None:
+        print("Sorry, but {0} needs a key to {1}".format(args.cipher, args.action))
+        exit(0)
+
+    if book[args.cipher].need_key:
+        temp_result = book[args.cipher].decrypt(text, args.key)
+    else:
+        temp_result = book[args.cipher].decrypt(text)
+
+    return temp_result
+
+
+def crack_action():
+    temp_result = book[args.cipher].crack(text)
+    return temp_result
+
+
+actions = {
+    "encrypt": encrypt_action,
+    "decrypt": decrypt_action,
+    "crack": crack_action
+}
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.cipher not in book:
         print("Sorry, but {0} is something I don't know".format(args.cipher))
-        exit(0)
-
-    if args.action not in book[args.cipher].actions:
-        print("Sorry, but I can't {0} {1}".format(args.action, args.cipher))
         exit(0)
 
     text = ""
@@ -29,28 +63,11 @@ if __name__ == "__main__":
         with open(args.inout_file) as input_:
             text = input_.read()
 
-    if args.action == "encrypt":
-        if book[args.cipher].need_key and args.key is None:
-            print("Sorry, but {0} needs a key to {1}".format(args.cipher, args.action))
-            exit(0)
-
-        if book[args.cipher].need_key:
-            result = book[args.cipher].encrypt(text, args.key)
-        else:
-            result = book[args.cipher].encrypt(text)
-
-    if args.action == "decrypt":
-        if book[args.cipher].need_key and args.key is None:
-            print("Sorry, but {0} needs a key to {1}".format(args.cipher, args.action))
-            exit(0)
-
-        if book[args.cipher].need_key:
-            result = book[args.cipher].decrypt(text, args.key)
-        else:
-            result = book[args.cipher].decrypt(text)
-
-    if args.action == "crack":
-        result = book[args.cipher].crack(text)
+    try:
+        result = actions[args.action]()
+    except KeyError:
+        print("Sorry, but I can't {0} {1}".format(args.action, args.cipher))
+        exit(0)
 
     if args.output_file is None:
         print("Output text:", result)
